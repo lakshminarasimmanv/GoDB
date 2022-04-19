8b36d39bae24a9a3a24aca2fba2a8c241b7c348a
Lakshmi Narasimman <lakshminarasimman.v17@gmail.com>
2022-04-19 21:23:46 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const Version = "1.0.1"
+
+type Address struct {
+	City    string
+	Door_No json.Number
+	Street  string
+	Country string
+	Pincode json.Number
+	State   string
+}
+
+type User struct {
+	Name    string
+	Age     json.Number
+	Contact string
+	Address Address
+}
+
+func main() {
+	dir := "./"
+
+	db, err := New(dir, nil)
+	if err != nil {
+		fmt.Printf("Error! %v\n", err)
+	}
+
+	employees := []User{
+		{"Lakshmi Narasimman", "22", "9453754225", Address{"Theni", "453", "AVR Compound", "India", "625531", "Tamil Nadu"}},
+		{"Kishore", "30", "9876543234", Address{"Mumbai", "742", "Main Road", "India", "625637", "Maharastra"}},
+	}
+
+	for _, value := range employees {
+		db.Write("users", value.Name, User{
+			Name:    value.Name,
+			Age:     value.Age,
+			Contact: value.Contact,
+			Address: value.Address,
+		})
+	}
+
+	records, err := db.ReadAll("users")
+	if err != nil {
+		fmt.Printf("Error! %v", err)
+	}
+	fmt.Printf("%v\n", records)
+
+}
+
+func New() {
+
+}
